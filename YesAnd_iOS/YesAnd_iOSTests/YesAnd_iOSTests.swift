0b3dffcab5a9603d72621bedac791bebf5bec791
Husein Kareem <huseinkareem@gmail.com>
2015-12-09 19:08:22 -0600

    initial commit

---
+++ YesAnd_iOS/YesAnd_iOSTests/YesAnd_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  YesAnd_iOSTests.swift
+//  YesAnd_iOSTests
+//
+//  Created by Husein Kareem on 12/9/15.
+//  Copyright © 2015 YesAnd. All rights reserved.
+//
+
+import XCTest
+@testable import YesAnd_iOS
+
+class YesAnd_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
